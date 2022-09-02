f13389b07f3ad710d418ad8dc63647f35f6ee0cf
Daud Dhiya <daud.dhiya31@gmail.com>
2022-09-02 14:55:51 +0700

    add main.py and readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# auotmate-emails
-program to send email automatically using py
+# automate-emails
+program to send gmail automatically using py
+
+source code : [Link](https://www.youtube.com/watch?v=g_j6ILT-X0k)
+
+## How to run :
+- clone the repo `git clone https://github.com/daudhiyaa/automate-emails.git`
+- customize these variables : 
+```
+- email_sender
+- email_receiver
+- subject
+- body
+```
+- Run the program `python3 main.py`
+- note : if you get an error in your `email_pass`, follow [this link](https://www.youtube.com/watch?v=g_j6ILT-X0k) to setup your `email_sender` password
+
+
+## Benefit using this code 
+use this code instead of send gmail manually if you need to send email without using your gmail
---
+++ main.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage
+from email.policy import SMTP
+import os
+import ssl
+import smtplib
+
+email_sender = 'youremail@gmail.com'
+email_pass = os.environ.get("EMAIL_PASS")
+email_receiver = ['youremail@gmail.com']
+
+subject = 'New Py Tutorial'
+body = '''
+you can access the new tutorial through the link :
+https://www.youtube.com/watch?v=g_j6ILT-X0k
+'''
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_pass)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
